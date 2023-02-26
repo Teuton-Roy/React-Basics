8c823cd3a6635af2e015ba6f00762afe37631f2d
Teuton Roy <popeyehk404@gmail.com>
2023-02-26 17:01:33 +0530

    add one image

--- basics/src/components/video.js
+++ basics/src/components/video.js
@@ -3,8 +3,12 @@ function Video(){
     return(
         <div>
             <h1>Video</h1>
+            <img src="https://cdn.pixabay.com/photo/2022/05/17/21/41/naruto-7203817__340.jpg" alt="" />
+            <div>
+                naruto
+            </div>
         </div>
     );
 }
 
-export default Video; 
+export default Video;  
