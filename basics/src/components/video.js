e624f9a744d06d8d4a54e2febf210140a457566c
Teuton Roy <popeyehk404@gmail.com>
2023-02-27 15:49:45 +0530

    add css around dark className.

--- basics/src/components/video.js
+++ basics/src/components/video.js
@@ -1,13 +1,14 @@
+import './video.css';
 
 function Video(){
     let topic = "Naruto's photo"
     let url = "http://placeimg.com/190/190/any"
-    let bg = "dark"
+    // let bg = "dark"
     return(
         <>
             {/* <h1>Video</h1> */}
             <img src={url} alt="" />
-            <div className = {bg}>
+            <div className = 'dark'>
                 {topic}
             </div>
         </>
