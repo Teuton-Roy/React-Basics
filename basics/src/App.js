98bd96cbb41d1346282ea81d3a392f5cd1faf89a
Teuton Roy <popeyehk404@gmail.com>
2023-02-28 18:36:16 +0530

    remove somethings

--- basics/src/App.js
+++ basics/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <div>
+      <div> 
         <Video channel="teuton.h" views="10K" time="1 year age" title="React.Js bootcamp"></Video>
         <Video channel="teuton.h" views="100K" time="3 weeks age" title="Node.Js bootcamp"></Video>
         <Video channel="teuton.h" views="25K" time="1 weeks age" title="MongoDB bootcamp"></Video>
