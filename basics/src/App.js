4ac693d286b7b62643c379c3cecb349807fa1f2a
Teuton Roy <popeyehk404@gmail.com>
2023-02-28 19:22:06 +0530

    done set to profile-image

--- basics/src/App.js
+++ basics/src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className='App'>
       <div>
-        <Video channel="teuton.h" views="10K" time="1 year ago" title="React.Js bootcamp"></Video>
-        <Video channel="teuton.h" views="100K" time="3 weeks ago" title="Node.Js bootcamp"></Video>
-        <Video channel="teuton.h" views="25K" time="1 weeks ago" title="MongoDB bootcamp"></Video>
+        <Video  channel="teuton.h" views="10K" time="1 year ago" title="React.Js bootcamp"></Video>
+        <Video  channel="teuton.h" views="100K" time="3 weeks ago" title="Node.Js bootcamp"></Video>
+        <Video  channel="teuton.h" views="25K" time="1 weeks ago" title="MongoDB bootcamp"></Video>
       </div>
     </div>
 
