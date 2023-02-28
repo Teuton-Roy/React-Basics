f2520c6d08e5587441fe0a96740f44be52127dc9
Teuton Roy <popeyehk404@gmail.com>
2023-02-28 18:04:35 +0530

    change some code

--- basics/src/components/video.js
+++ basics/src/components/video.js
@@ -1,16 +1,26 @@
 import './video.css';
 
-function Video({title, bgColor}){
-    // let topic = "Naruto's photo"
-    let url = "http://placeimg.com/190/190/any"
-    let bg = "dark"
+// function Video({title, bgColor}){
+//     // let topic = "Naruto's photo"
+//     let url = "http://placeimg.com/190/190/any"
+//     let bg = "dark"
+//     return(
+//         <>
+//             {/* <h1>Video</h1> */}
+//             <img src={url} alt="" />
+//             <div className={bg} style = {{backgroundColor:bgColor}} >
+//                 {title}
+//             </div>
+//         </>
+//     );
+// }
+
+function Video(){
     return(
         <>
-            {/* <h1>Video</h1> */}
-            <img src={url} alt="" />
-            <div className={bg} style = {{backgroundColor:bgColor}} >
-                {title}
-            </div>
+        <div className="container">
+            
+        </div>
         </>
     );
 }
