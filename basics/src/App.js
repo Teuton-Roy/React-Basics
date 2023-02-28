0b8fbbf7b3cd3e5dcf5fb53eebae16bf75b256df
Teuton Roy <popeyehk404@gmail.com>
2023-02-28 18:31:59 +0530

    ..

--- basics/src/App.js
+++ basics/src/App.js
@@ -4,15 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <div className="App">
-        {/* <h1>Hello</h1> */}
+    <div className='App'>
+      <div>
+        <Video channel="teuton.h" views="10K" time="1 year age" title="random generator photos_1"></Video>
+        <Video channel="teuton.h" views="100K" time="3 weeks age" title="random generator photos_2"></Video>
+        <Video channel="teuton.h" views="100K" time="3 weeks age" title="random generator photos_2"></Video>
       </div>
-    <Video channel="teuton.h" views="10K" time="1 year age" title="random generator photos_1"></Video>
-    <br/>
-    <Video channel="teuton.h" views="100K" time="3 weeks age" title="random generator photos_2"></Video>
     </div>
-    
+
   );
 }
 
