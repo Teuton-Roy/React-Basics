aa30a79b9536092822e34aa6be41e4199f901656
Teuton Roy <popeyehk404@gmail.com>
2023-02-28 18:38:30 +0530

    spelling mistake change

--- basics/src/App.js
+++ basics/src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <div> 
-        <Video channel="teuton.h" views="10K" time="1 year age" title="React.Js bootcamp"></Video>
-        <Video channel="teuton.h" views="100K" time="3 weeks age" title="Node.Js bootcamp"></Video>
-        <Video channel="teuton.h" views="25K" time="1 weeks age" title="MongoDB bootcamp"></Video>
+      <div>
+        <Video channel="teuton.h" views="10K" time="1 year ago" title="React.Js bootcamp"></Video>
+        <Video channel="teuton.h" views="100K" time="3 weeks ago" title="Node.Js bootcamp"></Video>
+        <Video channel="teuton.h" views="25K" time="1 weeks ago" title="MongoDB bootcamp"></Video>
       </div>
     </div>
 
